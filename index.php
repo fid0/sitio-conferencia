1a22542818d084e659641d42c579805d367efad0
F1D0 <fido.oops@gmail.com>
2015-08-03 15:28:56 -0400

    Se agrega contenido NUEVO al index.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Conferencia  SEGIC</title>
+</head>
+<body>
+<header>
+<h1> Conferencia de Desarrollo WEB</h1>
+<h3>Del 24 al 26 de Septiembre -  Santiago de Chile</h3>
+</header>
+
+<?php
+echo "<p>hell yeahhhh</p>";
+?>
+<section>
+	<h2>Dedicada a la construccion de sistemas y sitios webs</h2>
+	<p>Como cada año los desarrolladores mas brillantes de Santiago de Chile, discutiran sobre las Últimas tendencias tecnológicas. Acompáñanos este Septiembre</p>
+<section>
+	<section>
+		<h5>Relatores</h5>
+		<h3>Nos acompañaran desde todas los lugares del mundo, mas de 50 relarores especialistas, los cuales compartiran sus partes intimas con nosotros.</h5>
+<footer>
+	<p>SEGIC - USACH</p>
+</footer>
+		
+</body>
+</html>
